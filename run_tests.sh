59c3dff2ab125835f016a0cef1986cf6c76fb519
glebzlat <mrkotofey@inbox.ru>
2024-07-01 16:56:36 +0500

    Call the python command without saving output

--- run_tests.sh
+++ run_tests.sh
@@ -3,13 +3,11 @@
 for file in $(ls tests); do
   echo "Test $file"
 
-  result=$(python parser.py "tests/$file")
+  python parser.py "tests/$file"
   code=$?
 
   if [[ code -ne 0 ]]; then
     echo "  failed with code ${code}"
     exit 1
   fi
-
-  echo "  success with result: ${result}"
 done
